23d26126278f3b2219d138447e8b6fe385e962e5
Juan David Cruz Garcia <angrybirdsgallo@gmail.com>
2021-11-26 12:02:09 -0500

    Controller created

---
+++ src/ui/GameGUIController.java
@@ -0,0 +1,408 @@
+package ui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class GameGUIController {
+    @FXML
+    private Label GAMEhintsCounter;
+
+    @FXML
+    private Label GAMEtime;
+
+    @FXML
+    private Label GAMElb11;
+
+    @FXML
+    private Label GAMElb21;
+
+    @FXML
+    private Label GAMElb31;
+
+    @FXML
+    private Label GAMElb41;
+
+    @FXML
+    private Label GAMElb141;
+
+    @FXML
+    private Label GAMElb51;
+
+    @FXML
+    private Label GAMElb61;
+
+    @FXML
+    private Label GAMElb71;
+
+    @FXML
+    private Label GAMElb81;
+
+    @FXML
+    private Label GAMElb91;
+
+    @FXML
+    private Label GAMElb102;
+
+    @FXML
+    private Label GAMElb1011;
+
+    @FXML
+    private Label GAMElb12;
+
+    @FXML
+    private Label GAMElb22;
+
+    @FXML
+    private Label GAMElb32;
+
+    @FXML
+    private Label GAMElb42;
+
+    @FXML
+    private Label GAMElb142;
+
+    @FXML
+    private Label GAMElb52;
+
+    @FXML
+    private Label GAMElb62;
+
+    @FXML
+    private Label GAMElb72;
+
+    @FXML
+    private Label GAMElb82;
+
+    @FXML
+    private Label GAMElb92;
+
+    @FXML
+    private Label GAMElb103;
+
+    @FXML
+    private Label GAMElb1012;
+
+    @FXML
+    private Label GAMElb13;
+
+    @FXML
+    private Label GAMElb23;
+
+    @FXML
+    private Label GAMElb33;
+
+    @FXML
+    private Label GAMElb43;
+
+    @FXML
+    private Label GAMElb143;
+
+    @FXML
+    private Label GAMElb53;
+
+    @FXML
+    private Label GAMElb63;
+
+    @FXML
+    private Label GAMElb73;
+
+    @FXML
+    private Label GAMElb83;
+
+    @FXML
+    private Label GAMElb93;
+
+    @FXML
+    private Label GAMElb104;
+
+    @FXML
+    private Label GAMElb1013;
+
+    @FXML
+    private Label GAMElb15;
+
+    @FXML
+    private Label GAMElb24;
+
+    @FXML
+    private Label GAMElb34;
+
+    @FXML
+    private Label GAMElb44;
+
+    @FXML
+    private Label GAMElb144;
+
+    @FXML
+    private Label GAMElb54;
+
+    @FXML
+    private Label GAMElb64;
+
+    @FXML
+    private Label GAMElb74;
+
+    @FXML
+    private Label GAMElb84;
+
+    @FXML
+    private Label GAMElb94;
+
+    @FXML
+    private Label GAMElb105;
+
+    @FXML
+    private Label GAMElb1014;
+
+    @FXML
+    private Label GAMElb16;
+
+    @FXML
+    private Label GAMElb25;
+
+    @FXML
+    private Label GAMElb35;
+
+    @FXML
+    private Label GAMElb45;
+
+    @FXML
+    private Label GAMElb145;
+
+    @FXML
+    private Label GAMElb55;
+
+    @FXML
+    private Label GAMElb65;
+
+    @FXML
+    private Label GAMElb75;
+
+    @FXML
+    private Label GAMElb85;
+
+    @FXML
+    private Label GAMElb95;
+
+    @FXML
+    private Label GAMElb106;
+
+    @FXML
+    private Label GAMElb1015;
+
+    @FXML
+    private Label GAMElb17;
+
+    @FXML
+    private Label GAMElb26;
+
+    @FXML
+    private Label GAMElb36;
+
+    @FXML
+    private Label GAMElb46;
+
+    @FXML
+    private Label GAMElb146;
+
+    @FXML
+    private Label GAMElb56;
+
+    @FXML
+    private Label GAMElb66;
+
+    @FXML
+    private Label GAMElb76;
+
+    @FXML
+    private Label GAMElb86;
+
+    @FXML
+    private Label GAMElb96;
+
+    @FXML
+    private Label GAMElb107;
+
+    @FXML
+    private Label GAMElb1016;
+
+    @FXML
+    private Label GAMElb18;
+
+    @FXML
+    private Label GAMElb27;
+
+    @FXML
+    private Label GAMElb37;
+
+    @FXML
+    private Label GAMElb47;
+
+    @FXML
+    private Label GAMElb147;
+
+    @FXML
+    private Label GAMElb57;
+
+    @FXML
+    private Label GAMElb67;
+
+    @FXML
+    private Label GAMElb77;
+
+    @FXML
+    private Label GAMElb87;
+
+    @FXML
+    private Label GAMElb97;
+
+    @FXML
+    private Label GAMElb108;
+
+    @FXML
+    private Label GAMElb1017;
+
+    @FXML
+    private Label GAMElb19;
+
+    @FXML
+    private Label GAMElb28;
+
+    @FXML
+    private Label GAMElb38;
+
+    @FXML
+    private Label GAMElb48;
+
+    @FXML
+    private Label GAMElb148;
+
+    @FXML
+    private Label GAMElb68;
+
+    @FXML
+    private Label GAMElb78;
+
+    @FXML
+    private Label GAMElb88;
+
+    @FXML
+    private Label GAMElb98;
+
+    @FXML
+    private Label GAMElb109;
+
+    @FXML
+    private Label GAMElb1018;
+
+    @FXML
+    private Label GAMElb110;
+
+    @FXML
+    private Label GAMElb29;
+
+    @FXML
+    private Label GAMElb39;
+
+    @FXML
+    private Label GAMElb49;
+
+    @FXML
+    private Label GAMElb149;
+
+    @FXML
+    private Label GAMElb69;
+
+    @FXML
+    private Label GAMElb79;
+
+    @FXML
+    private Label GAMElb89;
+
+    @FXML
+    private Label GAMElb99;
+
+    @FXML
+    private Label GAMElb1010;
+
+    @FXML
+    private Label GAMElb1019;
+
+    @FXML
+    private Label GAMElb111;
+
+    @FXML
+    private Label GAMElb210;
+
+    @FXML
+    private Label GAMElb310;
+
+    @FXML
+    private Label GAMElb410;
+
+    @FXML
+    private Label GAMElb1410;
+
+    @FXML
+    private Label GAMElb610;
+
+    @FXML
+    private Label GAMElb710;
+
+    @FXML
+    private Label GAMElb810;
+
+    @FXML
+    private Label GAMElb910;
+
+    @FXML
+    private Label GAMElb1020;
+
+    @FXML
+    private Label GAMElb10110;
+
+    @FXML
+    private Label GAMElb112;
+
+    @FXML
+    private Label GAMElb211;
+
+    @FXML
+    private Label GAMElb311;
+
+    @FXML
+    private Label GAMElb411;
+
+    @FXML
+    private Label GAMElb58;
+
+    @FXML
+    private Label GAMElb611;
+
+    @FXML
+    private Label GAMElb711;
+
+    @FXML
+    private Label GAMElb811;
+
+    @FXML
+    private Label GAMElb911;
+
+    @FXML
+    private Label GAMElb1021;
+
+    @FXML
+    private Label GAMElb10111;
+
+    @FXML
+    void GAMEgiveUp(ActionEvent event) {
+
+    }
+
+    @FXML
+    void GAMEhint(ActionEvent event) {
+
+    }
+
+}
