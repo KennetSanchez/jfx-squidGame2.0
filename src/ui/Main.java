11e78c5c02843ed3b95ef5b21b97a5432375376f
Juan David Cruz Garcia <angrybirdsgallo@gmail.com>
2021-11-28 23:45:59 -0500

    Working in loading data

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -8,10 +8,23 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import model.Game;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
 public class Main extends Application {
 
-    private Game game = new Game();
-    private GameGUIController controller = new GameGUIController(game);
+    private Game game;
+    private GameGUIController controller;
+    private static final String GAME_PATH_FILE = "data/savedData.xd";
+
+    public Main(){
+        game=new Game();
+        loadGameData();
+        controller = new GameGUIController(game);
+    }
+
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -23,6 +36,20 @@ public class Main extends Application {
         primaryStage.setTitle("SquidGame 2.0");
         primaryStage.setResizable(false);
         primaryStage.show();
+    }
+
+    public void loadGameData() {
+        File f = new File(GAME_PATH_FILE);
+        if (f.exists()) {
+            ObjectInputStream ois;
+            try {
+                ois = new ObjectInputStream(new FileInputStream(f));
+                game = (Game) ois.readObject();
+                ois.close();
+            } catch (IOException | ClassNotFoundException e) {
+                System.out.println("File is empty or something else went wrong.");
+            }
+        }
     }
 
     @Override
